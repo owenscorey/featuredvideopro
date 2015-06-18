3ff635ea4299328ae9515d084c5f880ee06c5013
owenscorey <cowens@level8creative.cc>
2015-06-18 16:37:07 -0500

    Update and rename functions.php to featured-video-pro.php

---
+++ featured-video-pro.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Featured Video Pro
+Plugin URI: 
+Description: Video on-hover effect for your posts.
+Author: Level8 Creative
+Version: 1.0
+Author URI: http://www.level8creative.cc
+*/
+	
+?>
